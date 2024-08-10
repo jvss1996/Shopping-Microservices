f0831d0cccb29bc2bfc1b70aceb39275305c24f4
Jaivardhan <jvss1996@gmail.com>
2024-08-10 16:07:16 -0500

    added mongodb uri; added product class, repo, controller, service, request, response; added create, get products functions

---
+++ product-service/product-service/src/main/java/com/shekhawat/product/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.shekhawat.product.controller;
+
+import com.shekhawat.product.dto.ProductRequest;
+import com.shekhawat.product.dto.ProductResponse;
+import com.shekhawat.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
--- product-service/product-service/src/test/java/com/shekhawat/product/ProductServiceApplicationTests.java
+++ product-service/product-service/src/test/java/com/shekhawat/product/ProductServiceApplicationTests.java
@@ -3,13 +3,17 @@ package com.shekhawat.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // to run tests on random port, so that it doesn't conflict with main product service application
 class ProductServiceApplicationTests {
 
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+
 	}
 
 }
