1c64d1ac7e67f435b14504e0572f7dfd5585cda0
Jaivardhan <jvss1996@gmail.com>
2024-08-10 16:46:52 -0500

    added rest-assured library for testing, hardcoded mongo version, wrote test for checking product creation

--- product-service/product-service/src/test/java/com/shekhawat/product/ProductServiceApplicationTests.java
+++ product-service/product-service/src/test/java/com/shekhawat/product/ProductServiceApplicationTests.java
@@ -1,19 +1,53 @@
 package com.shekhawat.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Service;
 import org.testcontainers.containers.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // to run tests on random port, so that it doesn't conflict with main product service application
 class ProductServiceApplicationTests {
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+	@LocalServerPort
+	private Integer port;
 
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
 
 	@Test
 	void shouldCreateProduct() {
-
+		String requestBody = """
+				{
+				 	"name": "iPhone 16",
+				     "description": "Smartphone with Apple Intelligence",
+				     "price": 1000
+				}
+				""";
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 16"))
+				.body("description", Matchers.equalTo("Smartphone with Apple Intelligence"))
+				.body("price", Matchers.equalTo(1000));
 	}
 
 }
