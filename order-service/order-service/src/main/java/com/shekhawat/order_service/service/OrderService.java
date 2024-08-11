5fc13c72c58d9a02eddac65eb3eedd8a35c888e7
Jaivardhan <jvss1996@gmail.com>
2024-08-10 19:19:03 -0500

    added order class, repo, controller, service, request; created placeOrder function

---
+++ order-service/order-service/src/main/java/com/shekhawat/order_service/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.shekhawat.order_service.service;
+
+import com.shekhawat.order_service.dto.OrderRequest;
+import com.shekhawat.order_service.model.Order;
+import com.shekhawat.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest) {
+        // map OrderRequest to Order object
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        // save Order to order repository
+        orderRepository.save(order);
+    }
+}
