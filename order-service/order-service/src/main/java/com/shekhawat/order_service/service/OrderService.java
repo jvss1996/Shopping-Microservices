b68d045f9003b7f071b8a27f6f132cb8ff38f6d4
Jaivardhan <jvss1996@gmail.com>
2024-08-10 23:41:09 -0500

    used flyway for creation of orders table

--- order-service/order-service/src/main/java/com/shekhawat/order_service/service/OrderService.java
+++ order-service/order-service/src/main/java/com/shekhawat/order_service/service/OrderService.java
@@ -4,12 +4,14 @@ import com.shekhawat.order_service.dto.OrderRequest;
 import com.shekhawat.order_service.model.Order;
 import com.shekhawat.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
     public void placeOrder(OrderRequest orderRequest) {
@@ -21,5 +23,6 @@ public class OrderService {
         order.setQuantity(orderRequest.quantity());
         // save Order to order repository
         orderRepository.save(order);
+        log.info("Order placed successfully");
     }
 }
