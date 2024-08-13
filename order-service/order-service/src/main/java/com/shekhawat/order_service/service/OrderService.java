af65d6ecd383852e479d3318fcd2a09075723fd8
Jaivardhan <jvss1996@gmail.com>
2024-08-13 10:07:10 -0500

    installed Spring OpenFeign so that order service can communicate to inventory service, created inventory client, checking stock before placing order

--- order-service/order-service/src/main/java/com/shekhawat/order_service/service/OrderService.java
+++ order-service/order-service/src/main/java/com/shekhawat/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.shekhawat.order_service.service;
 
+import com.shekhawat.order_service.client.InventoryClient;
 import com.shekhawat.order_service.dto.OrderRequest;
 import com.shekhawat.order_service.model.Order;
 import com.shekhawat.order_service.repository.OrderRepository;
@@ -14,15 +15,23 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     public void placeOrder(OrderRequest orderRequest) {
-        // map OrderRequest to Order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        // save Order to order repository
-        orderRepository.save(order);
-        log.info("Order placed successfully");
+        var isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isInStock) {
+            // map OrderRequest to Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            // save Order to order repository
+            orderRepository.save(order);
+            log.info("Order placed successfully");
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is out of stock");
+        }
+
     }
 }
