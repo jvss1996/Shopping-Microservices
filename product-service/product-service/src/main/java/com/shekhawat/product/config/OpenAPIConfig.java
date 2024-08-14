c910be9e4fc2310caa54030d282375f31de522c5
Jaivardhan <jvss1996@gmail.com>
2024-08-14 14:43:48 -0500

    installed openapi ui and api for documenting api endpoints in ui and json format

---
+++ product-service/product-service/src/main/java/com/shekhawat/product/config/OpenAPIConfig.java
@@ -0,0 +1,24 @@
+package com.shekhawat.product.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI productServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Product Service API")
+                        .description("This is REST API for Product Service")
+                        .version("v1.0.1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Refer to the GitHub repository for more details")
+                        .url("https://github.com/springdoc/springdoc-openapi"));
+    }
+}
