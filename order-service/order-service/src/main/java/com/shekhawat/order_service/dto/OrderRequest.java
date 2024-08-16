ef99781ad3c9eca57f8042224c77f1cb9e0eeab9
Jaivardhan <jvss1996@gmail.com>
2024-08-16 16:21:30 -0500

    installed kafka, kafka ui, schema registry, zookeeper; created event schema; sending message to topic using kafka template while placing order

--- order-service/order-service/src/main/java/com/shekhawat/order_service/dto/OrderRequest.java
+++ order-service/order-service/src/main/java/com/shekhawat/order_service/dto/OrderRequest.java
@@ -2,5 +2,7 @@ package com.shekhawat.order_service.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity, UserDetails userDetails) {
+
+    public record UserDetails(String firstName, String lastName, String email) {}
 }
--- order-service/order-service/src/main/java/com/shekhawat/order_service/service/OrderService.java
+++ order-service/order-service/src/main/java/com/shekhawat/order_service/service/OrderService.java
@@ -2,10 +2,12 @@ package com.shekhawat.order_service.service;
 
 import com.shekhawat.order_service.client.InventoryClient;
 import com.shekhawat.order_service.dto.OrderRequest;
+import com.shekhawat.order_service.event.OrderPlacedEvent;
 import com.shekhawat.order_service.model.Order;
 import com.shekhawat.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,6 +18,7 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     public void placeOrder(OrderRequest orderRequest) {
         var isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
@@ -29,6 +32,12 @@ public class OrderService {
             // save Order to order repository
             orderRepository.save(order);
             log.info("Order placed successfully");
+
+            // Send message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber());
+            log.info("Starting sending OrderPlacedEvent: {}", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("Ending sending OrderPlacedEvent: {}", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is out of stock");
         }
